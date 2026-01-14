80f610a59794cc038f118ccae75358596a5021dc
Hasan Kamran <hasan8812011@hotmail.co.uk>
2026-01-14 11:41:16 +0000

    ci: add missing permissions for IAM

--- infra/modules/iam/cicd_terraform_apply_policy.tf
+++ infra/modules/iam/cicd_terraform_apply_policy.tf
@@ -5,6 +5,16 @@ resource "aws_iam_policy" "terraform_apply" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+
+      ##### TERRAFORM CORE READS #####
+      {
+        Sid    = "TerraformCallerIdentity"
+        Effect = "Allow"
+        Action = ["sts:GetCallerIdentity"]
+        Resource = "*"
+      },
+
+      ##### ECS #####
       {
         Sid    = "ECSDeploy"
         Effect = "Allow"
@@ -12,42 +22,71 @@ resource "aws_iam_policy" "terraform_apply" {
           "ecs:RegisterTaskDefinition",
           "ecs:DeregisterTaskDefinition",
           "ecs:UpdateService",
-          "ecs:DescribeServices",
-          "ecs:DescribeClusters",
-          "ecs:DescribeTaskDefinition",
-          "ecs:DescribeTasks",
           "ecs:TagResource",
           "ecs:UntagResource"
         ]
         Resource = "*"
       },
       {
-        Sid    = "ECRPushPull"
+        Sid    = "ECSReads"
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:DescribeClusters",
+          "ecs:DescribeTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:ListServices",
+          "ecs:ListTaskDefinitions"
+        ]
+        Resource = "*"
+      },
+
+      ##### ECR #####
+      {
+        Sid    = "ECRPush"
+        Effect = "Allow"
+        Action = [
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRReads"
         Effect = "Allow"
         Action = [
           "ecr:GetAuthorizationToken",
           "ecr:BatchGetImage",
           "ecr:GetDownloadUrlForLayer",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-          "ecr:PutImage",
           "ecr:DescribeRepositories",
           "ecr:BatchCheckLayerAvailability",
-          "ecr:ListTagsForResource"
+          "ecr:ListTagsForResource",
+          "ecr:DescribeImages",
+          "ecr:ListImages"
         ]
         Resource = "*"
       },
+
+      ##### TERRAFORM STATE #####
       {
-        Sid    = "TerraformState"
+        Sid    = "TerraformStateReads"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:PutObject",
           "s3:ListBucket"
         ]
         Resource = [
           "arn:aws:s3:::hasanenv-tf-state",
+          "arn:aws:s3:::hasanenv-tf-state/*"
+        ]
+      },
+      {
+        Sid    = "TerraformStateWrites"
+        Effect = "Allow"
+        Action = ["s3:PutObject"]
+        Resource = [
           "arn:aws:s3:::hasanenv-tf-state/*"
         ]
       },
@@ -61,19 +100,53 @@ resource "aws_iam_policy" "terraform_apply" {
         ]
         Resource = "arn:aws:dynamodb:eu-west-2:727646481331:table/terraform-state-lock"
       },
+
+      ##### ROUTE53 #####
       {
-        Sid    = "Route53Changes"
+        Sid    = "Route53Writes"
         Effect = "Allow"
         Action = [
-          "route53:ChangeResourceRecordSets",
-          "route53:ListHostedZones"
+          "route53:ChangeResourceRecordSets"
         ]
         Resource = "*"
       },
       {
-        Sid      = "ACMRead"
-        Effect   = "Allow"
-        Action   = ["acm:DescribeCertificate"]
+        Sid    = "Route53Reads"
+        Effect = "Allow"
+        Action = [
+          "route53:ListResourceRecordSets",
+          "route53:GetHostedZone",
+          "route53:ListHostedZones",
+          "route53:ListHostedZonesByName",
+          "route53:ListTagsForResource",
+          "route53domains:ListDomains"
+        ]
+        Resource = "*"
+      },
+
+      ##### ACM #####
+      {
+        Sid    = "ACMReads"
+        Effect = "Allow"
+        Action = [
+          "acm:DescribeCertificate",
+          "acm:ListCertificates",
+          "acm:ListTagsForCertificate"
+        ]
+        Resource = "*"
+      },
+
+      ##### IAM #####
+      {
+        Sid    = "IAMWrites"
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole",
+          "iam:CreatePolicy",
+          "iam:CreatePolicyVersion",
+          "iam:DeletePolicy",
+          "iam:DeletePolicyVersion"
+        ]
         Resource = "*"
       },
       {
@@ -82,41 +155,87 @@ resource "aws_iam_policy" "terraform_apply" {
         Action = [
           "iam:GetRole",
           "iam:GetPolicy",
+          "iam:GetPolicyVersion",
           "iam:ListPolicies",
-          "iam:PassRole"
+          "iam:ListPolicyVersions",
+          "iam:ListRoleTags",
+          "iam:ListRoles",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies"
+        ]
+        Resource = "*"
+      },
+
+      ##### LOGS #####
+      {
+        Sid    = "LogsWrites"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:PutRetentionPolicy"
         ]
         Resource = "*"
       },
       {
-        Sid    = "LogsWrite"
+        Sid    = "LogsReads"
         Effect = "Allow"
         Action = [
-          "logs:CreateLogGroup",
-          "logs:PutRetentionPolicy",
           "logs:DescribeLogGroups",
           "logs:ListTagsForResource"
         ]
         Resource = "*"
       },
+
+      ##### SSM #####
       {
-        Sid    = "SSMParameterWrites"
+        Sid    = "SSMWrites"
         Effect = "Allow"
         Action = [
           "ssm:PutParameter",
           "ssm:DeleteParameter",
           "ssm:AddTagsToResource",
-          "ssm:RemoveTagsFromResource"
+          "ssm:RemoveTagsFromResource",
+          "ssm:LabelParameterVersion"
         ]
         Resource = "*"
       },
       {
-        Sid    = "EC2Read"
+        Sid    = "SSMReads"
         Effect = "Allow"
         Action = [
-          "ec2:DescribeVpcs",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeRouteTables"
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath",
+          "ssm:DescribeParameters",
+          "ssm:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+
+      ##### EC2 #####
+      {
+        Sid    = "EC2Reads"
+        Effect = "Allow"
+        Action = ["ec2:Describe*"]
+        Resource = "*"
+      },
+
+      ##### ELB #####
+      {
+        Sid    = "ELBWrites"
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:ModifyListener",
+          "elasticloadbalancing:ModifyRule",
+          "elasticloadbalancing:ModifyTargetGroupAttributes"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ELBReads"
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:Describe*"
         ]
         Resource = "*"
       }
