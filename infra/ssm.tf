b0bdc9e82c3cd6f8f86151f87bd0100d438d17db
Hasan Kamran <hasan8812011@hotmail.co.uk>
2026-01-25 14:29:39 +0000

    chore(terraform): remove redundant ssm options

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -1,11 +1,7 @@
 resource "aws_ssm_parameter" "gatus_config" {
   name  = "/gatus/config"
-  type  = "SecureString"
+  type  = "String"
   value = file("${path.root}/config/config.yaml")
 
   tags = local.tags
-
-  depends_on = [
-    module.iam
-  ]
 }
