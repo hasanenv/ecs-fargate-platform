490ea08781be77aa191cac6e31102fc6a05cc9d7
Hasan Kamran <hasan8812011@hotmail.co.uk>
2026-02-03 14:23:27 +0000

    chore(ecr): set immutable tags and remove force delete line

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,6 @@
 resource "aws_ecr_repository" "gatus" {
   name                 = "gatus-repo"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
